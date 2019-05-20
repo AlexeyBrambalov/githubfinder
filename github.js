eca0cc38cfde2eefa9f18082546af0ca6becb5c5
Alexey Brambalov <brambalov@gmail.com>
2019-05-20 16:25:14 +0100

    last commit

--- github.js
+++ github.js
@@ -2,6 +2,8 @@ class Github {
   constructor() {
     this.client_id = "7d5b6892c7ec266d62ea";
     this.client_secret = "d037c5a08d5c959966af3a10f0ece31093fee508";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
@@ -11,7 +13,21 @@ class Github {
       }&client_secret=${this.client_secret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    };
 
     return {
       profile
